88e0b378dfa16ec202a53d00a3f7ea1c4d63e516
kendisiz <47818289+kendisiz@users.noreply.github.com>
2022-09-14 21:51:43 +0300

    2 files modified

--- src/app/async-test/async-test.component.ts
+++ src/app/async-test/async-test.component.ts
@@ -9,6 +9,7 @@ import { Observable } from 'rxjs';
 export class AsyncTestComponent implements OnInit {
   time = new Date().toUTCString();
   isPromiseResolved = false;
+  isObservableStarted = false;
   timeAsPromise = new Promise<String>((resolve, reject) => {
     setTimeout(() => {
       resolve(new Date().toUTCString());
@@ -17,8 +18,14 @@ export class AsyncTestComponent implements OnInit {
   });
 
   timeAsObservable = new Observable<String>((observer) => {
-    observer.next(new Date().toUTCString());
-    observer.complete();
+    let intervalRef = window.setInterval(() => {
+      this.isObservableStarted = true;
+      observer.next(new Date().toUTCString());
+    }, 1000);
+    window.setTimeout(() => {
+      window.clearInterval(intervalRef);
+      observer.complete();
+    }, 5000);
   });
 
   constructor() {}
